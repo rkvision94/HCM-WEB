83fa697b168918682a221c3245a23d2e4c8b05aa
drohitkumar <drohitkumar@192.168.0.108>
2021-04-22 21:22:06 +0530

    Added fields getter and setter

---
+++ rkvision1/src/main/java/com/spring/rkvision1/emp.java
@@ -0,0 +1,27 @@
+package com.spring.rkvision1;
+
+public class emp {
+
+	private String  name;
+	private String address;
+	private String Dept;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDept() {
+		return Dept;
+	}
+	public void setDept(String dept) {
+		Dept = dept;
+	}
+	
+}
